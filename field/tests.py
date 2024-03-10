7a75cab7ef345c17d07a53fde1a0a31a9928e468
sangily <sangilyoon.dev@gmail.com>
2024-03-10 22:22:06 +0900

    test: field

--- field/tests.py
+++ field/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from field.models import Fields
+
+
+class FieldAPITestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.field_model = Fields.objects.create(
+            name='Test Field'
+        )
+
+    def test_get_field(self):
+        url = reverse('getFieldInfo')
+        print(url)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
