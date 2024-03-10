3e028391dc3e727450df7594c877b31e8b8498cb
sangily <sangilyoon.dev@gmail.com>
2024-03-10 22:21:27 +0900

    env settings : hide secret keys

--- ct_board/settings.py
+++ ct_board/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
+
+if os.path.exists('env/'):
+    sys.path.append('env/')
+    if os.path.exists('env/secretKeys.py'):
+        from secretKeys import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pw0+_x^p6p^us*etrd^sv+m&s(o(m&9%j=k&*hegld9a@w*(vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,22 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ct_board.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASS'),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-        'PORT': os.environ.get('DATABASE_PORT', '3306'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
